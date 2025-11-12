4affaee9ab34f4ef32bc9b77edb9c4ead1b0b453
BeniSyach <benisyach32@gmail.com>
2025-11-13 01:17:33 +0700

    debug

--- app/Http/Controllers/Api/BerkasLainController.php
+++ app/Http/Controllers/Api/BerkasLainController.php
@@ -218,7 +218,7 @@ class BerkasLainController extends Controller
         $permohonan = BerkasLainModel::findOrFail($id);
 
         $filePath = $permohonan->nama_file_asli;
-
+        return $filePath;
         // Cek apakah file ada di disk public
         $disk = Storage::disk('public');
         if (!$disk->exists($filePath)) {
