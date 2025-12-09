867e8dded6bb73c4eede8dfe465fca94f4dcd5bb
BeniSyach <benisyach32@gmail.com>
2025-12-09 17:18:49 +0700

    update

--- app/Http/Controllers/Api/SP2D/SP2DController.php
+++ app/Http/Controllers/Api/SP2D/SP2DController.php
@@ -320,7 +320,7 @@ class SP2DController extends Controller
                       ->whereNotNull('diterima')
                       ->whereHas('sp2dkirim', function ($q) {
                           $q->whereNull('tgl_tte')
-                            ->whereNotNull('tte');
+                            ->whereNull('tte');
                       });
             
                 $FilterTanggal = 'tanggal_upload';
